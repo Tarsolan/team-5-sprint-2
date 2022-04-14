00d11ef8176c6ae782613e0c59d2fab7b4b1c592
Tarsolan <alex.ridgeley@keyin.com>
2022-04-14 12:41:13 -0230

    Login Stuff

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-const Login = () => {
+const Login = ({ logIn }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [captcha, toggleCaptcha] = useState(false);
+
+  const confirmLogin = (e) => {
+    e.preventDefault();
+    if (!captcha) {
+      alert("You are a robot. You are forbidden.");
+      return;
+    }
+    logIn(email, password);
+  };
+
   return (
     <div className="account-section-login">
       <h2>Log In</h2>
-      <form className="login-form">
+      <form className="login-form" onSubmit={confirmLogin}>
         <div className="form-group">
           <label htmlFor="username">Email Address</label>
           <input
@@ -13,15 +26,32 @@ const Login = () => {
             className="form-control"
             id="email"
             aria-describedby="emailHelp"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
           />
         </div>
         <div className="form-group">
           <label htmlFor="InputPassword2">Password</label>
-          <input type="password" className="form-control" id="InputPassword2" />
+          <input
+            type="password"
+            className="form-control"
+            id="InputPassword2"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
         </div>
         <br />
         <div className="form-check">
-          <input className="form-check-input" type="checkbox" id="gridCheck" />
+          <input
+            className="form-check-input"
+            type="checkbox"
+            id="gridCheck"
+            onChange={() => {
+              toggleCaptcha(!captcha);
+            }}
+          />
           <label className="form-check-label" htmlFor="gridCheck">
             I am not a robot
           </label>
