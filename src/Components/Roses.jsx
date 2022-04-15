87796aa3ed662f2b6293f4ea7f4eb502c3bdaff8
Tarsolan <alex.ridgeley@keyin.com>
2022-04-15 15:33:46 -0230

    Probably the last change for today

--- src/Components/Roses.jsx
+++ src/Components/Roses.jsx
@@ -7,27 +7,33 @@ export const Roses = ({ products, handleSelect }) => {
 
   return (
     <>
-      {Roses.map((item) => {
-        return (
-          <div className="mainGrid" key={item.id} onClick={handleSelect}>
-            <figure>
-              <img
-                src={item.image}
-                alt="image-JPG"
-                style={{ width: "200px" }}
-              />
-              {console.log(item.image)}
-            </figure>
-            <h2>{item.title}</h2>
-            <p>
-              {new Intl.NumberFormat("en-CA", {
-                style: "currency",
-                currency: "CAD",
-              }).format(item.price / 100)}
-            </p>
-          </div>
-        );
-      })}
+      <h2 className="category-head">Roses</h2>
+      <div className="gridContainer">
+        {Roses.map((item) => {
+          return (
+            <div
+              className="mainGrid"
+              key={item.id}
+              onClick={() => handleSelect(item)}
+            >
+              <figure>
+                <img
+                  src={item.image}
+                  alt="image-JPG"
+                  style={{ width: "200px" }}
+                />
+              </figure>
+              <h2>{item.title}</h2>
+              <p>
+                {new Intl.NumberFormat("en-CA", {
+                  style: "currency",
+                  currency: "CAD",
+                }).format(item.price / 100)}
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
