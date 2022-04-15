494f6b21299f2ca728d7af11a6650a47b29ec486
dBish6 <davidbish2002@hotmail.com>
2022-04-15 14:12:05 -0230

    ASS

--- src/Components/Lilies.jsx
+++ src/Components/Lilies.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 
 export const Lilies = ({ products }) => {
-  return <div>Roses</div>;
+  return (
+    <div className="background">
+      <div className="mainContainer">
+        <h1>Weapons... of love</h1>
+        <hr />
+
+        <div className="gridContainer">
+          {products.map((item) => {
+            if === item["c2"] return (
+              <div
+                className="mainGrid"
+                key={item.id}
+                onClick={goToAccountDetail}
+              >
+                <figure>
+                  <img
+                    src={item.image}
+                    alt="image-JPG"
+                    style={{ width: "200px" }}
+                  />
+                  {console.log(item.image)}
+                </figure>
+                <h2>{item.title}</h2>
+                <p>
+                  {new Intl.NumberFormat("en-CA", {
+                    style: "currency",
+                    currency: "CAD",
+                  }).format(item.price / 100)}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Lilies;
